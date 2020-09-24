05d832e513eb28b2e16dea29b22996beed00b740
CroStormShadow <ilijavuk14@gmail.com>
2020-09-25 00:38:18 +0200

    Fixed animation bug

--- Klon 2/Assets/Scripts/Game/Levels.cs
+++ Klon 2/Assets/Scripts/Game/Levels.cs
@@ -20,14 +20,15 @@ static class Levels
     static public int[,] levelTilemaps = new int[,]
     {
         { 0, 0, 1, 0, 0 }, // level 1
-        { 0, 1, 1, 1, 0 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
-        { 1, 1, 1, 1, 1 }, // level 1
+        { 0, 1, 1, 1, 0 }, // level 2
+        { 1, 1, 1, 1, 1 }, // level 3
+        { 1, 1, 1, 1, 1 }, // level 4
+        { 1, 1, 1, 1, 1 }, // level 5
+        { 1, 1, 1, 1, 1 }, // level 6
+        { 1, 1, 1, 1, 1 }, // level 7
+        { 1, 1, 1, 1, 1 }, // level 8
+        { 1, 1, 1, 1, 1 }, // level 9
+        { 1, 1, 1, 1, 1 }, // level 10
     };
+
 }
